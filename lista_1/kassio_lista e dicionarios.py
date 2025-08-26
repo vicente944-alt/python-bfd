6142951576a1e1aa54c982dcf163bed03d613610
kassiothomaz <k.thomaz@live.com>
2025-08-26 20:28:19 -0300

    exercicio de listae dicionario

---
+++ lista_1/kassio_lista e dicionarios.py
@@ -0,0 +1,11 @@
+lista_de_numeros=[]
+contagem ={}
+
+for x in range (4):
+    item_lista = int(input("Digite uns numeros para criar uma lista  :"))
+    contagem ={item_lista}
+    
+    
+    
+print (x)
+print (contagem)
