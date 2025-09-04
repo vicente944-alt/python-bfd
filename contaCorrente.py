f1062bc920764eba9d56143bb4ca051d4ff1515f
vicente944-alt <contaptestes07@gmail.com>
2025-09-04 19:45:45 -0300

    Update contaCorrente.py

--- contaCorrente.py
+++ contaCorrente.py
@@ -5,27 +5,34 @@ class ContaCorrente:
         self.saldo = saldo
 
     def addSaldo(self, valorAdd):
-        self.saldo = self.saldo + valorAdd
+        if valorAdd > 0:
+            self.saldo = self.saldo + valorAdd
+        else:
+            print("Valor inválido.")
 
     def sacaDinheiro(self, valorSac):
         if self.saldo >= valorSac:
-            self.saldo = self.saldo - valorSac
+            if valorSac > 0:
+                self.saldo = self.saldo - valorSac
+            else:
+                print("Valor inválido.")
         else:
             print("Saldo insuficiente.")
 
     def verifSaldo(self):
-        print(f"Saldo: {self.saldo}")
+        print(f"Saldo: R$ {self.saldo}")
 
 def main():
-    print("Conta Corrente")
-    conta = ContaCorrente("123.456.789-00", "Fulanelson", int(input("Saldo Incial: ")))
+    print("_____Conta Corrente_____")
+    conta = ContaCorrente("123.456.789-00", "Fulanelson Silva", int(input("Saldo Incial: R$ ")))
+    print(f"Nome: {conta.nomeC}\nCPF: {conta.cpf}")
     while True:
         print("1. Adicionar Saldo\n2. Sacar Dinheiro\n3. Verificar Saldo")
         acao = input('Digite o número da ação a ser realizada ou "/exit" para sair: ')
         if acao == "1" or acao == "1.":
-            conta.addSaldo(int(input("Quanto vai adicionar? ")))
+            conta.addSaldo(int(input("Quanto vai adicionar? R$ ")))
         elif acao == "2" or acao == "2.":
-            conta.sacaDinheiro(int(input("Quanto vai sacar? ")))
+            conta.sacaDinheiro(int(input("Quanto vai sacar? R$ ")))
         elif acao == "3" or acao == "3.":
             conta.verifSaldo()
         elif acao == "/exit":
