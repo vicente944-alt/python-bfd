937fc138460d002ea8f1a43a6a04d013773a1d97
vicente944-alt <contaptestes07@gmail.com>
2025-09-09 21:00:32 -0300

    Update ex05.py

--- ex05.py
+++ ex05.py
@@ -1,40 +1,48 @@
 class Produto:
-    def __init__(self, nome=str, preco=int, qntd=int):
-        self.nome = nome
+    def __init__(self, item, preco, qntd):
+        self.item = item
         self.preco = preco
         self.qntd = qntd
 
 
 class Estoque:
-    def __init__(self, produtos=list, nome=str, preco=int, qntd=int):
-        self.produtos = produtos
-        Produto.__init__(self, nome=str, preco=int, qntd=int)
+    def __init__(self, lista=[]):
+        self.lista = lista
 
-    def adicionar(self, nome):
-        self.produtos.append(nome)
+    def adicionar(self, item=Produto):
+        self.lista.append(item)
 
-    def remover(self, nome):
-        self.produtos.remove(nome)
+    def remover(self, item):
+        if item not in self.lista:
+            print("Isso não está na lista.")
+        else:
+            self.lista.remove(item)
 
     def listar(self):
-        print(self.produtos)
+        print(self.lista)
 
 
 def main():
     print("___Lista de produtos___")
     lista = Estoque()
     while True:
-        produto = Produto(input("Produto: "), 0, 0)
         print("1. Adicionar produto\n2. Remover produto\n3. Verificar lista")
         entrada = input('Digite o número da ação ou digite "/exit" para parar: ')
+
         if entrada == "1" or entrada == "1.":
-            lista.adicionar(produto.nome)
+            produto = Produto(input("Produto: ").title(), 0, 0)
+            lista.adicionar(produto.item)
+
         elif entrada == "2" or entrada == "2.":
-            lista.remover(produto.nome)
+            produto = Produto(input("Produto: ").title(), 0, 0)
+            lista.remover(produto.item)
+
         elif entrada == "3" or entrada == "3.":
             lista.listar()
+
         elif entrada == "/exit":
             break
+
         else:
             print("Comando inválido.")
 
